df6ec004fc4ffb8937ee058ba547823a2c4c2db7
jgage930 <jgage930@gmail.com>
2025-07-24 09:44:54 -0500

    hello api

--- api/__main__.py
+++ api/__main__.py
@@ -1,1 +1,20 @@
-print("Testing commit")
+from fastapi import FastAPI
+import uvicorn
+from . import __version__
+
+
+def init_app() -> FastAPI:
+    app = FastAPI()
+    return app
+
+
+app = init_app()
+
+
+@app.get("/")
+def root():
+    return {"msg": f"Online Ordering v{__version__}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("__main__:app", port=5000, reload=True)
