cbc6c18510b698c17a0be05721b3e1c3a3db74cf
jgage930 <jgage930@gmail.com>
2025-07-24 10:54:56 -0500

    add username

--- api/database.py
+++ api/database.py
@@ -62,6 +62,7 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String, unique=True, index=True)
     email: Mapped[str] = mapped_column(String, unique=True, index=True)
     password_hash: Mapped[str] = mapped_column(String)
 
