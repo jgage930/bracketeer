fa0b26f7c6254ec4b762d8b7c98ff7112203568d
jgage930 <jgage930@gmail.com>
2025-07-30 09:43:04 -0500

    auth working

--- api/auth/encrypt.py
+++ api/auth/encrypt.py
@@ -1,5 +1,17 @@
 import hashlib
+from jose import jwt
+import os
 
 
 def hash_password(password: str) -> str:
     return hashlib.sha256(password.encode()).hexdigest()
+
+
+def encode_token(username: str) -> str:
+    return jwt.encode(
+        {"username": username}, key=os.environ["SECRET_KEY"], algorithm="HS256"
+    )
+
+
+def decode_token(token: str) -> dict:
+    return jwt.decode(token, os.environ["SECRET_KEY"], algorithms=["HS256"])
--- api/auth/router.py
+++ api/auth/router.py
@@ -4,78 +4,34 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 
-from api.auth.encrypt import hash_password
+from api.auth.encrypt import hash_password, encode_token, decode_token
 from api.auth.user import get_user_by_username
 from api.database import Database
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
-        "disabled": False,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "disabled": True,
-    },
-}
-
-
-def fake_hash_password(password: str):
-    return "fakehashed" + password
+from api.utils import into_pydantic
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 auth_router = APIRouter(tags=["auth"])
 
 
-class User(BaseModel):
+class CurrentUser(BaseModel):
+    id: int
+    email: str
     username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
 
 
-class UserInDB(User):
-    hashed_password: str
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Database):
+    token = decode_token(token)
+    user = await get_user_by_username(db, token["username"])
 
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
-def fake_decode_token(token):
-    # This doesn't provide any security at all
-    # Check the next version
-    user = get_user(fake_users_db, token)
-    return user
-
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid authentication credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    return user
 
-
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)],
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-
-    return current_user
+    return into_pydantic(user, CurrentUser)
 
 
 @auth_router.post("/token")
@@ -90,11 +46,12 @@ async def login(
     if not hashed_password == user.password_hash:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
 
-    return {"access_token": user.username, "token_type": "bearer"}
+    token = encode_token(user.username)
+    return {"access_token": token, "token_type": "bearer"}
 
 
 @auth_router.get("/users/me")
 async def read_users_me(
-    current_user: Annotated[User, Depends(get_current_active_user)],
+    current_user: Annotated[dict, Depends(get_current_user)],
 ):
     return current_user
